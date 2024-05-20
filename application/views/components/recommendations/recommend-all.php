ca84b329866b4ce2589adb9d7d180dfa7b64ae32
pim <pornthipsaefung@gmail.com>
2024-05-20 16:24:28 +0700

    feat: add title recommend all

--- application/views/components/recommendations/recommend-all.php
+++ application/views/components/recommendations/recommend-all.php
@@ -5,18 +5,31 @@ include ('recommend-list-tickets.php');
 include ('recommend-list-hotels.php');
 ?>
 <div class="bg-white">
-  <div class="container py-4">
-    <div class="row g-3">
-      <div class="col-12">
-        <?php include ('recommend-html-activities.php'); ?>
-      </div>
-      <div class="col-12">
-        <?php include ('recommend-html-package.php'); ?>
-      </div>
-      <div class="col-12">
-        <?php include ('recommend-html-ticket.php'); ?>
-      </div>
-      <div class="col-12"><?php include ('recommend-html-hotel.php'); ?></div>
+    <div class="container py-4">
+        <div class="row g-3">
+            <div class="col-12">
+                <div class="pb-3 fs-5 fw-bold text-dark">
+                    <?php echo getTS('_Activities', $lang, $mlangs); ?>
+                </div>
+                <?php include ('recommend-html-activities.php'); ?>
+            </div>
+            <div class="col-12">
+                <div class="pb-3 fs-5 fw-bold text-dark">
+                    <?php echo getTS('_Packages', $lang, $mlangs); ?>
+                </div>
+                <?php include ('recommend-html-package.php'); ?>
+            </div>
+            <div class="col-12">
+                <div class="pb-3 fs-5 fw-bold text-dark">
+                    <?php echo getTS('_Tickets', $lang, $mlangs); ?>
+                </div>
+                <?php include ('recommend-html-ticket.php'); ?>
+            </div>
+            <div class="col-12">
+                <div class="pb-3 fs-5 fw-bold text-dark">
+                    <?php echo getTS('_Hotels', $lang, $mlangs); ?>
+                </div><?php include ('recommend-html-hotel.php'); ?>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
