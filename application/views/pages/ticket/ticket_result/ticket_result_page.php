4afa9d9c2afa31ddda1ec250c860d206f9a889b6
pim <pornthipsaefung@gmail.com>
2024-05-21 09:50:23 +0700

    feat: edit html ticket result

--- application/views/pages/ticket/ticket_result/ticket_result_page.php
+++ application/views/pages/ticket/ticket_result/ticket_result_page.php
@@ -160,7 +160,7 @@
         <div class="pt-3">
             <div class="border bg-white rounded-2 p-3">
                 <div class="row gy-3 gx-4">
-                    <div class="col-12 col-md-4 col-lg-4">
+                    <div class="col-12 col-md-3 col-lg-3">
                         <div class="rn-imgcover-center rounded-2"
                             style="background-image: url('https://www.jtbtrip.com/ImageData/Place/keisei-skyliner-e-ticket-th--ma2403000001-general1.jpg');">
                             <a href="<?php echo '/activities/description/downtown-osaka-walking-and-local-food-tour/TUEyMzAxMDAwMDQw' . $getURLLang; ?>"
@@ -169,7 +169,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col-12 col-md-5 col-lg-5">
+                    <div class="col-12 col-md-6 col-lg-6">
                         <div>
                             <div class="pb-2">
                                 <a href="<?php echo '/activities/description/downtown-osaka-walking-and-local-food-tour/TUEyMzAxMDAwMDQw' . $getURLLang; ?>"
@@ -183,156 +183,29 @@
                             </div>
                         </div>
 
-                        <?php
-                            for ($j = 1; $j <= rand(1, 3); $j++) {
-                                ?>
-                        <div>
-                            <div class="py-3 border-top">
-                                <div class="row g-3">
-                                    <div class="col-12 col-lg-9">
-                                        <div class="d-flex w-100 justify-content-between">
-                                            <div class="w-100">
-                                                <button class="btn border-0 p-0 text-start fs-7 fw-bold text-dark"
-                                                    data-bs-toggle="collapse"
-                                                    data-bs-target="<?php echo '#collapseDetail' . $i . $j; ?>"
-                                                    aria-expanded="false">
-                                                    [2024] 1-Day Kyoto & Nara Tour (No Meals, Round-trip
-                                                    from Osaka)
-                                                </button>
-                                            </div>
-                                            <div class="">
-                                                <div
-                                                    class="border border-primary text-primary fs-9 py-1 px-3 rounded-pill">
-                                                    Available
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-lg-3">
-                                        <div class="row">
-                                            <div class="col-8">
-                                                <button class="btn border-0 p-0 fs-7 w-100 text-dark fw-bold text-end">
-                                                    CAD 105.27
-                                                </button>
-                                            </div>
-                                            <div class="col-4">
-                                                <button class="btn btn-primary rounded-1 border-0 p-0 fs-7 w-100"
-                                                    data-bs-toggle="collapse"
-                                                    data-bs-target="<?php echo '#collapseDetail' . $i . $j; ?>"
-                                                    aria-expanded="false">
-                                                    <i class="fas fa-angle-down"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
+
+                    </div>
+                    <div class="col-12 col-md-3 col-lg-3 border-start">
+                        <div class="">
+                            <div class="pt-3 px-3 fs-8 text-secondary text-start text-md-center">
+                                Start From
+                            </div>
+                            <div class="pt-0 px-3">
+                                <div class="text-title text-dark fw-bold text-start text-md-center">
+                                    <small class="pe-1 fs-6">THB</small>
+                                    <span class="ps-0 fs-4 fw-bold"> 1,425 </span>
                                 </div>
                             </div>
-
-                            <div class="collapse" id="<?php echo 'collapseDetail' . $i . $j; ?>">
-                                <div class="pb-3 border-top">
-                                    <div class="row g-3">
-                                        <div class="col-12 col-lg-9">
-                                            <div class="pt-3">
-                                                <div class="row g-2">
-                                                    <div class="col-auto">
-                                                        <div
-                                                            class="border border-primary py-1 px-3 fs-8 fw-bold text-primary rounded-1">
-                                                            Seat in coach
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-auto">
-                                                        <div class="border border-secondary py-1 px-3 fs-8 text-secondary rounded-1"
-                                                            title="Thing to do code">
-                                                            205412
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-auto">
-                                                        <div class="border border-secondary py-1 px-3 fs-8 text-secondary rounded-1"
-                                                            title="Tour">
-                                                            ALL DAY
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-auto">
-                                                        <div class="border border-secondary py-1 px-3 fs-8 text-secondary rounded-1"
-                                                            title="Start time">
-                                                            08:00
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="pt-3">
-                                                <div class="row">
-                                                    <div class="col-auto">
-                                                        <button class="btn border-0 p-0" data-bs-toggle="modal"
-                                                            data-bs-target="#modalDesc">
-                                                            <div class="d-flex">
-                                                                <div class="pe-2 text-secondary fs-7"><i
-                                                                        class="fas fa-info-circle"></i>
-                                                                </div>
-                                                                <div
-                                                                    class="ps-0 text-secondary fs-7 text-decoration-underline">
-                                                                    View Description</div>
-                                                            </div>
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="pt-3">
-                                                <div class="fs-7 text-secondary">
-                                                    Minimum Person(s) : 1 Person(s)
-                                                </div>
-                                                <div class="fs-7 text-secondary">
-                                                    Starting from : Osaka
-                                                </div>
-                                                <div class="fs-7 text-secondary">
-                                                    Duration : 4 Hours
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-lg-3">
-                                            <div class="pt-3">
-                                                <div class="row g-2">
-                                                    <div class="col-12">
-                                                        <div class="border bg-light rounded-1 px-2 py-1">
-                                                            <div class="text-center fs-8 text-dark">1
-                                                                Adult x CAD 105.27
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12">
-                                                        <div class="border bg-light rounded-1 px-2 py-1">
-                                                            <div class="text-center fs-8 text-dark">0
-                                                                Child x CAD 0.00</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="pt-3">
-                                                <button
-                                                    class="btn btn-outline-primary w-100 fs-7 fw-bold rounded-1 py-2">
-                                                    View Detail
-                                                </button>
-                                            </div>
-                                            <div class="pt-3">
-                                                <button class="btn p-0 fs-8 text-danger border-0 shadow-none">
-                                                    <div class="d-flex">
-                                                        <div class="pe-2"><i class="fas fa-exclamation-circle"></i>
-                                                        </div>
-                                                        <div> Cancellation policy </div>
-                                                    </div>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <div class="pt-2 px-3 text-start text-md-center">
+                                <div class="fs-8 text-secondary">
+                                    Room/Night (Included taxes &amp; fees)
                                 </div>
+                            </div>
+                            <div class="pt-3 px-3">
+                                <button class="btn py-2 btn-sm w-100 btn-primary">View Rates</button>
                             </div>
                         </div>
-                        <?php
-                            }
-                            ?>
-                    </div>
-                    <div class="col-12 col-md-3 col-lg-3">
-                        xxxxx
+
                     </div>
                 </div>
             </div>
