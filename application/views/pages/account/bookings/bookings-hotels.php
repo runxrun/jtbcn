d4df57986e0192006fb42a5a53252502f42e7a66
pim <pornthipsaefung@gmail.com>
2024-06-11 13:20:21 +0700

    feat: add data account booking

--- application/views/pages/account/bookings/bookings-hotels.php
+++ application/views/pages/account/bookings/bookings-hotels.php
@@ -67,3 +67,217 @@
         </div>
     </div>
 </div>
+
+
+<div id="Cancelled" aria-hidden="true" class="border bg-white overflow-hidden rounded mt-3" style="">
+    <div class="d-flex flex-wrap">
+        <div class="col-12 col-md-4 col-lg-3 p-0">
+            <div class="d-flex h-100 w-100"
+                style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
+                <div class="img-hotel h-100 w-100"
+                    style="background: url(&quot;/application/images/recommend/activity/japan-3664500_1280.jpg&quot;) center center / cover no-repeat;">
+                    <img src="/assets/images/image-ratio-4-3.gif" class="w-100">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 col-lg-9 p-0">
+            <div class="pt-3 px-3">
+                <div class="row">
+                    <div class="pb-3 col-12 col-lg-7 col-xl-8">
+                        <div class="pb-2 fs-8 text-dark fw-bold">
+                            <span class="text-primary pr-2"> Confirm No. : </span>
+                            <span class="text-primary"> B2CMMA240300014 </span>
+                        </div>
+                        <div class="pb-3 fs-7 fw-bold text-dark">
+                            Downtown Osaka Walking and Local Food Tour
+                        </div>
+
+                        <div class="pb-3">
+                            <div class="fs-8 text-success d-flex">
+                                <span class="pr-2"><i class="fas fa-check-circle"></i></span>
+                                <span class="fw-bold"> Cancel Confirmed </span>
+                            </div>
+
+                        </div>
+                        <div class="fs-8 text-secondary">
+                            Booked Date: : 19 Mar 2024 </div>
+                        <div class="fs-8 text-secondary"> Service
+                            Type : Activity
+                        </div>
+
+                    </div>
+                    <div class="pb-3 col-12 col-lg-5 col-xl-4">
+
+                        <div class="d-flex">
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    in </div>
+                                <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    out </div>
+                                <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                        </div>
+
+
+                        <div class="pt-3"><button class="btn btn-primary fs-8 py-2 w-100"
+                                onclick="window.open('/account/bookings-detail', '_parent')"> View
+                                Detail
+                            </button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div id="Cancelled" aria-hidden="true" class="border bg-white overflow-hidden rounded mt-3" style="">
+    <div class="d-flex flex-wrap">
+        <div class="col-12 col-md-4 col-lg-3 p-0">
+            <div class="d-flex h-100 w-100"
+                style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
+                <div class="img-hotel h-100 w-100"
+                    style="background: url(&quot;http://jtbcn/application/images/recommend/jrpass/jr-west-japan-1342780_1280.jpg&quot;) center center / cover no-repeat;">
+                    <img src="/assets/images/image-ratio-4-3.gif" class="w-100">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 col-lg-9 p-0">
+            <div class="pt-3 px-3">
+                <div class="row">
+                    <div class="pb-3 col-12 col-lg-7 col-xl-8">
+                        <div class="pb-2 fs-8 text-dark fw-bold">
+                            <span class="text-primary pr-2"> Confirm No. : </span>
+                            <span class="text-primary"> B2CMMA240300014 </span>
+                        </div>
+                        <div class="pb-3 fs-7 fw-bold text-dark">
+                            JR West All Area Pass 7 Days – for JR-WEST All Area Pass 7 days
+                        </div>
+
+                        <div class="pb-3">
+                            <div class="fs-8 text-success d-flex">
+                                <span class="pr-2"><i class="fas fa-check-circle"></i></span>
+                                <span class="fw-bold"> Cancel Confirmed </span>
+                            </div>
+
+                        </div>
+                        <div class="fs-8 text-secondary">
+                            Booked Date: : 19 Mar 2024 </div>
+                        <div class="fs-8 text-secondary"> Service
+                            Type : Japan Rail Pass
+                        </div>
+
+                    </div>
+                    <div class="pb-3 col-12 col-lg-5 col-xl-4">
+
+                        <div class="d-flex">
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    in </div>
+                                <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    out </div>
+                                <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                        </div>
+
+
+                        <div class="pt-3"><button class="btn btn-primary fs-8 py-2 w-100"
+                                onclick="window.open('/account/bookings-detail', '_parent')"> View
+                                Detail
+                            </button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="Cancelled" aria-hidden="true" class="border bg-white overflow-hidden rounded mt-3" style="">
+    <div class="d-flex flex-wrap">
+        <div class="col-12 col-md-4 col-lg-3 p-0">
+            <div class="d-flex h-100 w-100"
+                style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
+                <div class="img-hotel h-100 w-100"
+                    style="background: url(&quot;https://www.jtbtrip.com/ImageData/Place/keisei-skyliner-e-ticket-th--ma2403000001-general1.jpg&quot;) center center / cover no-repeat;">
+                    <img src="/assets/images/image-ratio-4-3.gif" class="w-100">
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 col-lg-9 p-0">
+            <div class="pt-3 px-3">
+                <div class="row">
+                    <div class="pb-3 col-12 col-lg-7 col-xl-8">
+                        <div class="pb-2 fs-8 text-dark fw-bold">
+                            <span class="text-primary pr-2"> Confirm No. : </span>
+                            <span class="text-primary"> B2CMMA240300014 </span>
+                        </div>
+                        <div class="pb-3 fs-7 fw-bold text-dark">
+                            Keisei Skyliner (E-Ticket)
+                        </div>
+
+                        <div class="pb-3">
+                            <div class="fs-8 text-success d-flex">
+                                <span class="pr-2"><i class="fas fa-check-circle"></i></span>
+                                <span class="fw-bold"> Cancel Confirmed </span>
+                            </div>
+
+                        </div>
+                        <div class="fs-8 text-secondary">
+                            Booked Date: : 19 Mar 2024 </div>
+                        <div class="fs-8 text-secondary"> Service
+                            Type : Tickets
+                        </div>
+
+                    </div>
+                    <div class="pb-3 col-12 col-lg-5 col-xl-4">
+
+                        <div class="d-flex">
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    in </div>
+                                <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-6 p-0">
+                                <div class="fs-9 text-secondary text-truncate">
+                                    Check
+                                    out </div>
+                                <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
+                                <div class="fs-9 text-secondary text-truncate"> May
+                                    2024 </div>
+                            </div>
+                        </div>
+
+
+                        <div class="pt-3"><button class="btn btn-primary fs-8 py-2 w-100"
+                                onclick="window.open('/account/bookings-detail', '_parent')"> View
+                                Detail
+                            </button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
