bffdd03b7296aa1c374e0c9aea41631adabb179f
pim <pornthipsaefung@gmail.com>
2024-06-12 11:12:17 +0700

    feat: edit code html account bookings

--- application/views/pages/account/bookings/bookings-hotels.php
+++ application/views/pages/account/bookings/bookings-hotels.php
@@ -1,121 +1,27 @@
-<div id="Cancelled" aria-hidden="true" class="border border-2 bg-white overflow-hidden rounded mt-3" style="">
+<div id="Cancelled" aria-hidden="true" class="border  bg-white overflow-hidden rounded mt-3" style="">
   <div class="d-flex flex-wrap">
     <div class="col-12 col-md-12 col-lg-12 p-0">
-      <div class="pt-3 px-3">
+      <div class="">
         <div class="row">
-          <div class="pb-3 col-12 col-lg-7 col-xl-4">
-            <div class="pb-2 fs-7 text-dark fw-bold">
+          <div class="col-12 col-lg-7 m-3">
+            <div class="fs-7 text-dark fw-bold pe-2">
               <span class="text-primary pe-2"> Confirm No. : </span>
               <span class="text-primary"> B2CMMA240300014 </span>
             </div>
-
-            <div class="pb-2">
-              <div class="fs-8 text-success d-flex">
-                <span class="pe-2"><i class="fas fa-check-circle"></i></span>
-                <span class="fw-bold"> Cancel Confirmed </span>
-              </div>
-              <div class="fs-9 text-secondary">
-                Booked Date: : 19 Mar 2024 </div>
-            </div>
-
           </div>
-
-          <div class="pb-3 col-12 col-lg-5 col-xl-4">
-
-            <div class="d-flex text-center">
-              <div class="border-start"></div>
-              <div class="col-6 col-md-4 col-lg-6 p-0">
-                <div class="fs-9 text-secondary text-truncate">
-                  Check
-                  in </div>
-                <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
-                <div class="fs-9 text-secondary text-truncate"> May
-                  2024 </div>
-              </div>
-              <div class="col-6 col-md-4 col-lg-6 p-0">
-                <div class="fs-9 text-secondary text-truncate">
-                  Check
-                  out </div>
-                <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
-                <div class="fs-9 text-secondary text-truncate"> May
-                  2024 </div>
-              </div>
-              <div class="border-end"></div>
+          <div class="col-12 col-lg-4 m-3 text-end">
+            <div class="fs-7 text-success  ">
+              <span class="pe-2"><i class="fas fa-check-circle"></i></span>
+              <span class="fw-bold"> Cancel Confirmed </span>
             </div>
           </div>
-
-          <div class="pb-3 col-12 col-lg-5 col-xl-4">
-
-            <div class="pt-3"><button class="btn btn-primary fs-8 py-2 w-100"
-                onclick="window.open('/account/bookings-detail', '_parent')"> View
-                Detail
-              </button></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div>
-    <!-- <div class="border-top pb-3"></div> -->
-    <div class="d-flex flex-wrap m-3">
-      <div class="pb-3 col-12 col-md-3 col-lg-1 p-0">
-        <div class="d-flex h-100 w-100"
-          style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
-          <div class="img-hotel w-100 rounded"
-            style="background: url(&quot;/application/images/recommend/activity/japan-3664500_1280.jpg&quot;) center center / cover no-repeat;">
-            <img src="/assets/images/image-ratio-1-1.gif">
-          </div>
-        </div>
-      </div>
-      <div class="pb-3 col-12 col-lg-7 col-xl-8 ps-3">
-        <div class="pb-2 fs-7 fw-bold text-dark"><i class="fas fa-running"></i>
-          Hotelbox Demo (SHA Extra Plus)
         </div>
 
-        <div class="fs-8 text-secondary"> Service
-          Type : Activity
-        </div>
-      </div>
-
-
-    </div>
-
-    <div class="d-flex flex-wrap m-3">
-      <div class="pb-3 col-12 col-md-4 col-lg-1 p-0">
-        <div class="d-flex h-100 w-100"
-          style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
-          <div class="img-hotel h-100 w-100 rounded"
-            style="background: url(&quot;https://manage.infotechhotelbox.com/ImageData/Hotel/hotelbox_demo_%28sha_extra_plus%29-general1.jpg&quot;) center center / cover no-repeat;">
-            <img src="/assets/images/image-ratio-1-1.gif">
-          </div>
-        </div>
-      </div>
-
-      <div class="pb-3 col-12 col-lg-7 col-xl-8 ps-3">
-        <div class="pb-2 fs-7 fw-bold text-dark"><i class="fas fa-hotel"></i>
-          Downtown Osaka Walking and Local Food Tour
-        </div>
-        <div class="fs-8 text-secondary"> Service
-          Type : Hotel
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div id="Cancelled" aria-hidden="true" class="border border-2 bg-white overflow-hidden rounded mt-3" style="">
-  <div class="d-flex flex-wrap">
-    <div class="col-12 col-md-12 col-lg-12 p-0">
-      <div class="pt-3 px-3">
-        <div class="row">
+        <div class="row  border-top">
 
 
           <div class="pb-3 col-12 col-lg-5 col-xl-8">
-            <div class="pb-2 fs-7 text-dark fw-bold">
-              <span class="text-primary pe-2"> Confirm No. : </span>
-              <span class="text-primary"> B2CMMA240300014 </span>
-            </div>
+
             <div class="d-flex flex-wrap m-3">
               <div class="pb-3 col-12 col-md-3 col-lg-2 p-0">
                 <div class="d-flex h-100 w-100">
@@ -126,33 +32,17 @@
                 </div>
               </div>
               <div class="pb-3 col-12 col-lg-7 col-xl-10 ps-3">
-                <div class="pb-2 fs-7 fw-bold text-dark">
-                  Hotelbox Demo (SHA Extra Plus)
+                <div class="pb-1 fs-7 fw-bold text-dark"> Downtown Osaka Walking and Local Food Tour
                 </div>
 
-                <div class="fs-8 text-secondary"> Service
-                  Type : Activity
-                </div>
+                <div class="fs-8 text-secondary"> Service Type : Activity</div>
 
-                <div class="d-flex pt-3">
+                <div class="d-flex pt-2">
                   <!-- <div class="border-start"></div> -->
-                  <div class="col-6 col-md-4 col-lg-6 p-0">
-                    <div class="fs-9 text-secondary text-truncate">
-                      Check
-                      in </div>
-                    <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
-                    <div class="fs-9 text-secondary text-truncate"> May
-                      2024 </div>
+                  <div class="col-6 col-md-4 col-lg-4 p-0">
+                    <div class="fs-9 text-secondary text-truncate"> Check in </div>
+                    <div class="fs-8 fw-bold text-dark"> Wed 15 May 2024</div>
                   </div>
-                  <div class="col-6 col-md-4 col-lg-6 p-0">
-                    <div class="fs-9 text-secondary text-truncate">
-                      Check
-                      out </div>
-                    <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
-                    <div class="fs-9 text-secondary text-truncate"> May
-                      2024 </div>
-                  </div>
-                  <!-- <div class="border-end"></div> -->
                 </div>
               </div>
 
@@ -171,30 +61,20 @@
               </div>
 
               <div class="pb-3 col-12 col-lg-7 col-xl-10 ps-3">
-                <div class="pb-2 fs-7 fw-bold text-dark">
-                  Downtown Osaka Walking and Local Food Tour
+                <div class="pb-1 fs-7 fw-bold text-dark">
+                  Hotelbox Demo (SHA Extra Plus)
                 </div>
-                <div class="fs-8 text-secondary"> Service
-                  Type : Hotel
-                </div>
+                <div class="fs-8 text-secondary"> Service Type : Hotel</div>
 
-                <div class="d-flex pt-3">
+                <div class="d-flex pt-2">
                   <!-- <div class="border-start"></div> -->
-                  <div class="col-6 col-md-4 col-lg-6 p-0">
-                    <div class="fs-9 text-secondary text-truncate">
-                      Check
-                      in </div>
-                    <div class="fs-7 fw-bold text-dark"> Wed 15 </div>
-                    <div class="fs-9 text-secondary text-truncate"> May
-                      2024 </div>
+                  <div class="col-6 col-md-4 col-lg-4 p-0">
+                    <div class="fs-9 text-secondary text-truncate"> Check in </div>
+                    <div class="fs-8 fw-bold text-dark"> Wed 15 May 2024</div>
                   </div>
-                  <div class="col-6 col-md-4 col-lg-6 p-0">
-                    <div class="fs-9 text-secondary text-truncate">
-                      Check
-                      out </div>
-                    <div class="fs-7 fw-bold text-dark"> Fri 17 </div>
-                    <div class="fs-9 text-secondary text-truncate"> May
-                      2024 </div>
+                  <div class="col-6 col-md-4 col-lg-4 p-0">
+                    <div class="fs-9 text-secondary text-truncate"> Check out </div>
+                    <div class="fs-8 fw-bold text-dark"> Fri 17 May 2024 </div>
                   </div>
                   <!-- <div class="border-end"></div> -->
                 </div>
@@ -204,74 +84,99 @@
           </div>
 
 
-
-          <div class="pb-3 col-12 col-lg-5 col-xl-4">
-            <div class="pb-2 text-center">
-              <div class="fs-8 text-success  text-center">
-                <span class="pe-2"><i class="fas fa-check-circle"></i></span>
-                <span class="fw-bold"> Cancel Confirmed </span>
-              </div>
-              <div class="fs-9 text-secondary">
-                Booked Date: : 19 Mar 2024 </div>
+          <div class="pb-3 col-12 col-lg-5 col-xl-4 pt-3 px-4 rn-border-start">
+            <div class="">
+              <button class="btn btn-primary fs-8 py-2 w-100"
+                onclick="window.open('/account/bookings-detail', '_parent')">
+                View Detail
+              </button>
             </div>
 
+            <div class="pb-4 text-center pt-3 bg-light">
 
-
-            <div class="pt-3"><button class="btn btn-primary fs-8 py-2 w-100"
-                onclick="window.open('/account/bookings-detail', '_parent')"> View
-                Detail
-              </button></div>
+              <div class="fs-8 text-secondary"> Booked Date: : 19 Mar 2024 </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 
-  <div>
-    <!-- <div class="border-top pb-3"></div> -->
-    <!-- <div class="d-flex flex-wrap m-3">
-            <div class="pb-3 col-12 col-md-3 col-lg-1 p-0">
-                <div class="d-flex h-100 w-100"
-                    style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
-                    <div class="img-hotel w-100 rounded"
-                        style="background: url(&quot;/application/images/recommend/activity/japan-3664500_1280.jpg&quot;) center center / cover no-repeat;">
-                        <img src="/assets/images/image-ratio-1-1.gif">
-                    </div>
-                </div>
+<div id="Cancelled" aria-hidden="true" class="border  bg-white overflow-hidden rounded mt-3" style="">
+  <div class="d-flex flex-wrap">
+    <div class="col-12 col-md-12 col-lg-12 p-0">
+      <div class="">
+        <div class="row">
+          <div class="col-12 col-lg-7 m-3">
+            <div class="fs-7 text-dark fw-bold pe-2">
+              <span class="text-primary pe-2"> Confirm No. : </span>
+              <span class="text-primary"> B2CMMA240300015 </span>
             </div>
-            <div class="pb-3 col-12 col-lg-7 col-xl-8 ps-3">
-                <div class="pb-2 fs-7 fw-bold text-dark"><i class="fas fa-running"></i>
-                    Hotelbox Demo (SHA Extra Plus)
-                </div>
-
-                <div class="fs-8 text-secondary"> Service
-                    Type : Activity
-                </div>
+          </div>
+          <div class="col-12 col-lg-4 m-3 text-end">
+            <div class="fs-7 text-success  ">
+              <span class="pe-2"><i class="fas fa-check-circle"></i></span>
+              <span class="fw-bold"> Cancel Confirmed </span>
             </div>
-
-
+          </div>
         </div>
 
-        <div class="d-flex flex-wrap m-3">
-            <div class="pb-3 col-12 col-md-4 col-lg-1 p-0">
+        <div class="row  border-top">
+
+
+          <div class="pb-3 col-12 col-lg-5 col-xl-8">
+
+            <div class="d-flex flex-wrap m-3">
+              <div class="pb-3 col-12 col-md-4 col-lg-2 p-0">
                 <div class="d-flex h-100 w-100"
-                    style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
-                    <div class="img-hotel h-100 w-100 rounded"
-                        style="background: url(&quot;https://manage.infotechhotelbox.com/ImageData/Hotel/hotelbox_demo_%28sha_extra_plus%29-general1.jpg&quot;) center center / cover no-repeat;">
-                        <img src="/assets/images/image-ratio-1-1.gif">
-                    </div>
+                  style="background: url(&quot;assets/images/no-picture.svg&quot;) center center / cover no-repeat;">
+                  <div class="img-hotel h-100 w-100 rounded"
+                    style="background: url(&quot;https://manage.infotechhotelbox.com/ImageData/Hotel/hotelbox_demo_%28sha_extra_plus%29-general1.jpg&quot;) center center / cover no-repeat;">
+                    <img src="/assets/images/image-ratio-1-1.gif">
+                  </div>
                 </div>
+              </div>
+
+              <div class="pb-3 col-12 col-lg-7 col-xl-10 ps-3">
+                <div class="pb-1 fs-7 fw-bold text-dark">
+                  Hotelbox Demo (SHA Extra Plus)
+                </div>
+                <div class="fs-8 text-secondary"> Service Type : Hotel</div>
+
+                <div class="d-flex pt-2">
+                  <!-- <div class="border-start"></div> -->
+                  <div class="col-6 col-md-4 col-lg-4 p-0">
+                    <div class="fs-9 text-secondary text-truncate"> Check in </div>
+                    <div class="fs-8 fw-bold text-dark"> Wed 15 May 2024</div>
+                  </div>
+                  <div class="col-6 col-md-4 col-lg-4 p-0">
+                    <div class="fs-9 text-secondary text-truncate"> Check out </div>
+                    <div class="fs-8 fw-bold text-dark"> Fri 17 May 2024 </div>
+                  </div>
+                  <!-- <div class="border-end"></div> -->
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div class="pb-3 col-12 col-lg-5 col-xl-4 pt-3 px-4 rn-border-start">
+            <div class="">
+              <button class="btn btn-primary fs-8 py-2 w-100"
+                onclick="window.open('/account/bookings-detail', '_parent')">
+                View Detail
+              </button>
             </div>
 
-            <div class="pb-3 col-12 col-lg-7 col-xl-8 ps-3">
-                <div class="pb-2 fs-7 fw-bold text-dark"><i class="fas fa-hotel"></i>
-                    Downtown Osaka Walking and Local Food Tour
-                </div>
-                <div class="fs-8 text-secondary"> Service
-                    Type : Hotel
-                </div>
-            </div>
+            <div class="pb-4 text-center pt-3 bg-light">
 
-        </div> -->
+              <div class="fs-8 text-secondary"> Booked Date: : 19 Mar 2024 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
